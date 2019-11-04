0f048e3e3a62f9c6461a487b4e13dc5271f47005
jack seymour <stilljack@gmail.com>
2019-11-04 13:21:05 -0800

    ok there's by name length.
    the answer is 24 (how long the longest name was)

--- src/main/java/com/example/whatever/countries/CountryEnpointsRestController.java
+++ src/main/java/com/example/whatever/countries/CountryEnpointsRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 @RestController
 @RequestMapping("/data")
@@ -19,7 +20,7 @@ public class CountryEnpointsRestController {
     public ResponseEntity<?> getAllCountries()
     {
         CountriesApplication.ourCountriesList.countryList.sort(
-                (c1,c2)->c1.getName().compareToIgnoreCase(c2.getName()));
+                getAlphabetizer());
                 return new ResponseEntity<>(CountriesApplication.ourCountriesList.countryList, HttpStatus.OK);
 
     }
@@ -34,8 +35,64 @@ public class CountryEnpointsRestController {
                         Character.toUpperCase(letter)
         );
 
-        rtnCountries.sort((c1,c2)->c1.getName().compareToIgnoreCase(c2.getName()));
+        rtnCountries.sort(getAlphabetizer());
         return new ResponseEntity<>(rtnCountries,HttpStatus.OK);
     }
+    
+
+/*
+ /names/size/{number}
+return the countries alphabetically that have a name equal to or longer than the given length
+/population/size/{people}
+*/
+
+@GetMapping(value = "/countries/bynamelength/{length}",
+produces ={"application/json"})
+    public ResponseEntity<?> getCountriesByNameLength (@PathVariable int length)
+{
+    ArrayList<Country> rtnCountries = CountriesApplication.ourCountriesList.findCountries(
+            c-> c.getName().length()
+                    >=
+                    length
+    );
+
+    rtnCountries.sort(getAlphabetizer());
+    return new ResponseEntity<>(rtnCountries,HttpStatus.OK);
+    
+    
+    
+    
+}
+
+    private Comparator<Country> getAlphabetizer() {
+        return (c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName());
+    }
+
+
+/*
+return the countries that have a population equal to or greater than the given population
+/population/min
+
+return the country with the smallest population
+/population/max
+
+return the country with the largest population
+    Stretch Goal
+
+/population/median
+return the country with the median population
+/age/age/{age}
+
+return the countries that have a median age equal to or greater than the given age
+/age/min
+
+return the country with the smallest median age
+/age/max
+
+return the country the the greatest median age
+    Stretch Goal
+
+/age/median
+return the country with the median median age*/
 
 }
